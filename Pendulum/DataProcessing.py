b07a48b0dfc06ba60cec5f32910efb3ffc673b56
Oliver Liebe <Oliverliebe@hotmail.com>
2022-12-05 12:59:26 +0100

    Commit

--- Pendulum/DataProcessing.py
+++ Pendulum/DataProcessing.py
@@ -37,8 +37,6 @@ def weighted_error_prop_mean(var_list, sig_list, name):                         
 # Define a symbolic function for calculation the period with Sympy, which helps calculate error propagation in symbolic expressions
 
 def period_Pendulum(val_prev, val_prev_sig, val_now, val_now_sig):                      # Assuming the period and length are uncorrelated
-    if val_prev == nan or val_now == nan:
-        sys.exit()
     x, y = symbols("x, y")                                                              # Assigning symbolic nature to variables
     dx, dy = symbols("sigma_x, sigma_y")                                                # Assigning symbolic nature to variables
     T = x - y                                                                           # Defining the function for the expression
@@ -54,8 +52,6 @@ def period_Pendulum(val_prev, val_prev_sig, val_now, val_now_sig):              
 
 
 def length_addition(Length, Length_sig, Weight_length, Weight_length_sig):              # Assuming the period and length are uncorrelated
-    if Length == nan or Weight_length == nan:
-        sys.exit
     x, y = symbols("x, y")                                                              # Assigning symbolic nature to variables
     dx, dy = symbols("sigma_x, sigma_y")                                                # Assigning symbolic nature to variables
     L = x + (1/2)*y                                                                     # Defining the function for the expression
@@ -66,15 +62,12 @@ def length_addition(Length, Length_sig, Weight_length, Weight_length_sig):      
     vWeight_length, vdWeight_length = Weight_length, Weight_length_sig                  # Insert values for second parameter
     vL = fL(vLength, vWeight_length)                                                    # Calculate the numerical function
     vdL = fdL(vLength, vdLength, vWeight_length, vdWeight_length)                       # Calculate the numerical function of the uncertainty
-    print("The calculated period with propagated errors is: " f"{vL:.2f}", "+-", f"{vdL:.2f}")
+    #print("The calculated length with propagated errors is: " f"{vL:.2f}", "+-", f"{vdL:.2f}")
     return vL, vdL
 
 # Define a symbolic function for g, to calculate error prop in symbolic functions
 
 def g_Pendulum(T, T_sig, L, L_sig):                                 # Assuming the period and length are uncorrelated
-    print(T)
-    if T == nan or L == nan:
-        sys.exit()
     x, y = symbols("x, y")                                          # Assigning symbolic nature to variables
     dx, dy = symbols("sigma_x, sigma_y")                            # Assigning symbolic nature to variables
     g = x*(2*np.pi/y)**2                                            # Defining the function for the expression
@@ -85,7 +78,7 @@ def g_Pendulum(T, T_sig, L, L_sig):                                 # Assuming t
     vT, vdT = T, T_sig                                              # Insert values for second parameter
     vg = fg(vL, vT)                                                 # Calculate the numerical function
     vdg = fdg(vL, vdL, vT, vdT)                                     # Calculate the numerical function of the uncertainty
-    print("The calculated gravitional velocity with propagated errors is: " f"{vg:.2f}", "+-", f"{vdg:.2f} using {L} as the length")
+    print("The calculated gravitional velocity with propagated errors is: " f"{vg:.2f}", "+-", f"{vdg:.2f} using {L:.2f} as the length")
     return vg, vdg
 
 # Start working on the data-processing of the timers of the pendulum
@@ -94,37 +87,38 @@ def g_Pendulum(T, T_sig, L, L_sig):                                 # Assuming t
 
 path_to_timer_dat = str(os.getcwd() + r"/alex_output_1.dat")
 path_to_data = str(os.getcwd() + r"/PendulumData.csv")
+path_to_T_raw_sig = str(os.getcwd() + r"/T_raw_sig.csv")
 
 file = np.loadtxt(path_to_timer_dat)                                                        # Load the .dat file
 array_of_times = np.zeros(len(file))                                                        # Set a clean numpy array to load the data into
 for i in range(len(file)):                                                                  # Iterate through the .dat file
     array_of_times[i] = float(list(str(file[i][:]).split(".   ", 1))[1].split("]")[0])      # This handles the weird format into clean floats
 
-
-df = pd.read_csv(path_to_data)                                                              # Pandas to read the .csv file.
+df = pd.read_csv(path_to_data) 
+df_T_raw_sig = pd.read_csv(path_to_T_raw_sig)                                                             # Pandas to read the .csv file.
 
 #for i in range(len(df.columns)):
 #    df.iloc[i].dropna()
 
-T_raw_sig = df["T_raw_sig (s)"].tolist()                                                    # Pandas read sigs from data file
-
-print(len(T_raw_sig), len(array_of_times))
+T_raw_sig = df_T_raw_sig["T_raw_sig (s)"].tolist()                                                    # Pandas read sigs from data file
 
 T = np.zeros(len(array_of_times) - 1)                                                       # Initialise period array
 T_sig = np.zeros(len(array_of_times) - 1)                                                   # Initialise period_sig array
 for i in range(1, len(array_of_times)):                                                     # Iterate to calculate T and T_sig, using previous function
     T[i - 1], T_sig[i - 1] = period_Pendulum(array_of_times[i - 1], T_raw_sig[i-1], array_of_times[i], T_raw_sig[i])
 
+Length_las = np.zeros(len(df["w_h (m)"]))
+Length_las_sig = np.zeros(len(df["w_h (m)"]))
 Length = np.zeros(len(df["w_h (m)"]))
 Length_sig = np.zeros(len(df["w_h (m)"]))
 for i in range(len(df["w_h (m)"])):
-    Length[i], Length_sig[i] = length_addition(df["Len_las (m)"][i], df["Len_las_sig (m)"][i], df["w_h (m)"][i], df["w_h_sig (m)"][i])
+    Length_las[i], Length_las_sig[i] = length_addition(df["Len_las (m)"][i], df["Len_las_sig (m)"][i], df["w_h (m)"][i], df["w_h_sig (m)"][i])
+    Length[i], Length_sig[i] = length_addition(df["Len (m)"][i], df["Len_sig (m)"][i], df["w_h (m)"][i], df["w_h_sig (m)"][i])
 
-print(Length)
 
 T_mean, T_sig_mean = weighted_error_prop_mean(T, T_sig, "period")                                    # Combine values to get weighted mean and error prop of T
-L_las_mean, L_las_mean_sig = weighted_error_prop_mean_df(df["Len_las (m)"], df["Len_las_sig (m)"])
-L_mean, L_mean_sig = weighted_error_prop_mean_df(df["Len (m)"], df["Len_sig (m)"])
+L_las_mean, L_las_mean_sig = weighted_error_prop_mean(Length_las, Length_las_sig, "L_laser")
+L_mean, L_mean_sig = weighted_error_prop_mean(Length, Length_sig, "L")
 g_las = g_Pendulum(T_mean, T_sig_mean, L_las_mean, L_las_mean_sig)
 g = g_Pendulum(T_mean, T_sig_mean, L_mean, L_mean_sig)
 #print("The value of the gravitional acceleration, with no error propagation, is: "f"{g:.3f}")
