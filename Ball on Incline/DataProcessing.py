abd28304bbde04e1877bc1fdc0335a0b30eec33d
Arnulle <62622872+Arnulle@users.noreply.github.com>
2022-12-10 15:51:29 +0100

    Update DataProcessing.py
    Added a script part which loads the data and finds the edges of peaks.
    TODO: Need to do a chi2 fit and combine all to g

--- Ball on Incline/DataProcessing.py
+++ Ball on Incline/DataProcessing.py
@@ -3,6 +3,8 @@ import numpy as np
 import sympy
 from sympy import *
 import os
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 # Define function to calculate the error propagation in collected data using a weighted mean ------------------------------------------------
 
@@ -58,3 +60,70 @@ def angle(Hyp, Opp, Hyp_uncern, Opp_uncern):                        # Assuming t
 
 theta = angle(Hyp_mean, h_mean, Hyp_uncert, h_uncert)
 
+#We load the time data for Big and small spheres into two arrays, as well as two for the reverse cases
+
+#Since we have an uneven number of data points, we drop the first data points from each array untill they all have length of the shortest
+#We can check that we haven't lost any peaks and so there won't be a problem.
+
+
+Datlengs=np.zeros(10)
+for i in range(10):
+    Lenload=np.genfromtxt(os.getcwd()+"/Balls/Original/big_sphere_%s.csv"%(i+1),delimiter=',',skip_header=15).T  #Skips the header and transposes so first axis is shortest (ie. data along columns)
+    Datlengs[i]=len(Lenload[0])                                                                                  #Records the lengths of each data set
+minlen=int(np.amin(Datlengs))                                                                                    #Finds the dataset of smallest length
+
+Big_Sphere_O=np.zeros((10,2,minlen))                                                                               #Defines an array with length of the smallest set
+for i in range(10):
+    Inload=np.genfromtxt(os.getcwd()+"/Balls/Original/big_sphere_%s.csv"%(i+1),delimiter=',',skip_header=15).T   #Loads a dataset
+    Big_Sphere_O[i,:,:]=Inload[:,len(Inload[0])-minlen:]                                                           #Only uses the last minlen entries in each set
+
+#with the same methos, we then load the same data for the small case and then the reverse cases for each.
+Datlengs=np.zeros(10)
+for i in range(10):
+    Lenload=np.genfromtxt(os.getcwd()+"/Balls/Original/small_sphere_%s.csv"%(i+1),delimiter=',',skip_header=15).T  
+    Datlengs[i]=len(Lenload[0])                                                                                 
+minlen=int(np.amin(Datlengs))                                                                                    
+
+Small_Sphere_O=np.zeros((10,2,minlen))                                                                               
+for i in range(10):
+    Inload=np.genfromtxt(os.getcwd()+"/Balls/Original/small_sphere_%s.csv"%(i+1),delimiter=',',skip_header=15).T   
+    Small_Sphere_O[i,:,:]=Inload[:,len(Inload[0])-minlen:]  
+
+#Reverse big
+Datlengs=np.zeros(10)
+for i in range(10):
+    Lenload=np.genfromtxt(os.getcwd()+"/Balls/Reverse/big_rev_sphere_%s.csv"%(i+1),delimiter=',',skip_header=15).T  
+    Datlengs[i]=len(Lenload[0])                                                                                 
+minlen=int(np.amin(Datlengs))                                                                                    
+
+Big_Sphere_R=np.zeros((10,2,minlen))                                                                               
+for i in range(10):
+    Inload=np.genfromtxt(os.getcwd()+"/Balls/Reverse/big_rev_sphere_%s.csv"%(i+1),delimiter=',',skip_header=15).T   
+    Big_Sphere_R[i,:,:]=Inload[:,len(Inload[0])-minlen:] 
+
+#Small rev sphere
+Datlengs=np.zeros(10)
+for i in range(10):
+    Lenload=np.genfromtxt(os.getcwd()+"/Balls/Reverse/small_rev_sphere_%s.csv"%(i+1),delimiter=',',skip_header=15).T  
+    Datlengs[i]=len(Lenload[0])                                                                                 
+minlen=int(np.amin(Datlengs))                                                                                    
+
+Small_Sphere_R=np.zeros((10,2,minlen))                                                                               
+for i in range(10):
+    Inload=np.genfromtxt(os.getcwd()+"/Balls/Reverse/small_rev_sphere_%s.csv"%(i+1),delimiter=',',skip_header=15).T   
+    Small_Sphere_R[i,:,:]=Inload[:,len(Inload[0])-minlen:]   
+
+#Next we identify the peaks to find the time values for each pass
+def Peaksfromarr(A):                                                                                            #This function takes an array with gate data along axis1 and N experiments along axis0 and returns the right edges of the peaks
+    redge_time=np.zeros((A.shape[0],5))
+    for i in range(A.shape[0]):
+        mask=(A[i,1,:]>=4)                                                                                       #By only looking at values above 4 we sort out the rough area around the peaks
+        Peaks=np.vstack((A[i,0,:][mask],A[i,1,:][mask]))                                                         #Then we apply the mask to both the time and voltage data
+        dx=(np.roll(Peaks[0,:],-1)-Peaks[0,:])                                                                 #We then look at the difference in measured times between each element and it's neighbour.
+        redges=Peaks[0][abs(dx)>=0.05]                                                                               #The right-side edges of each peak will have a larger value and so we find them like this
+        if (len(redges)>5):
+            redges=redges[:-1]                                                                                   #Removes the last element if there are too many peaks. This is if the ball bounced back into a gate.
+        redge_time[i,:]=redges
+    return redge_time                                                                                            #It feels bad to not give an uncertainty. Technically I guess this should be related to the extension of the slope for the peak sides, but it seems negligible from the plots and will be dominated by other errors regardless.
+
+#Only thing remaining is to combine the time data with length to fit an a and then combine all into a measure of g
